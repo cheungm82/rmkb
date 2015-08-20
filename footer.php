c1008900e654289ef602b5c440a7746eb81ed426
cheungm82 <cheungm82@gmail.com>
2015-08-20 22:37:51 +0700

    combined into one HTML

--- footer.php
+++ footer.php
@@ -1,17 +1,8 @@
-			<footer>
-				<div class="row">
-					<div class="small-12 columns">&copy; 2015 RMKB Ventures. All Rights Reserved</div>
-<!-- 					<div class="small-4 columns">FAQ TERMS PRIVACY BLOG</div>
-					<div class="small-2 columns">
-						<i class="fa fa-facebook"></i>
-						<i class="fa fa-twitter"></i>
-						<i class="fa fa-instagram"></i>
-						<i class="fa fa-pinterest"></i>
-					</div>
-				</div> -->
-			</footer>
-			<a class="exit-off-canvas"></a>
-		</div></div>
+		<footer>
+			<div class="row">
+				<div class="small-12 columns">&copy; 2015 RMKB Ventures. All Rights Reserved</div>
+			</div>
+		</footer>
 		<!-- /// Jquery ////////  -->
 		<script src="js/vendor/jquery.js"></script>
 
@@ -19,21 +10,11 @@
 		<script src="js/vendor/fastclick.js"></script>
 		<script src="js/foundation.min.js"></script>
 
-		<!-- /// Parallax ////////  -->
-		<!-- <script src="js/vendor/skrollr.js"></script> -->
-
 		<!-- /// Viewport ////////  -->
 		<script src="js/vendor/jquery.viewport.js"></script>
 
 		<!-- /// Fonts ////////  -->
 		<script src="js/vendor/liga.js"></script>
-
-
-
-
-<!--	<script src="js/vendor/jquery.waypoints.min.js"></script> -->
-<!-- 	<script src="js/vendor/imagesloaded.pkgd.min.js"></script> -->
-
 		<script src="js/sprinkle.js"></script>
 	</body>
 </html>
